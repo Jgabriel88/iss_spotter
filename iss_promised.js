a637a0d8b87bd0e75e8ecd6a325f82dfdae201d3
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-12 00:31:25 +0000

    promisse

--- iss_promised.js
+++ iss_promised.js
@@ -7,14 +7,14 @@ const fetchMyIP = () => {
 const fetchCoordsByIP = (body) => {
   const ip = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${ip}`);
-}
+};
 
 const fetchISSFlyOverTimes = (body) => {
   const {latitude, longitude} = JSON.parse(body);
   return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
-}
+};
 
-const nextISSTimesForMyLocation = function () {
+const nextISSTimesForMyLocation = function() {
   return fetchMyIP()
     .then(fetchCoordsByIP)
     .then(fetchISSFlyOverTimes)
